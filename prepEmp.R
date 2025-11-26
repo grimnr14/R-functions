8fb7ff31f70f23e9c78249d05f15afe50824d65b
Christopher Kitchen <chrisk2@umbc.edu>
2025-11-26 08:49:10 -0500

    Update prepEmp.R

--- prepEmp.R
+++ prepEmp.R
@@ -251,7 +251,7 @@ prepEmp<-function(year=2020,geography="tract"){
       es<-merge(lodes,map[,c("GEOID","ZCTA5")],by.x="geoid",by.y="GEOID")
       es<-es[!duplicated(es)&!is.na(es$ZCTA5),]
       es[is.na(es)]<-0
-      es<-es[,!names(es) %in% c("GEOID","geoid","year","state")]%>%
+      es<-es[,!names(es) %in% c("GEOID","geoid","geography","year","state")]%>%
         group_by(ZCTA5)%>%
         summarise_each(funs=c(sum))
       
@@ -262,11 +262,12 @@ prepEmp<-function(year=2020,geography="tract"){
       
       es<-merge(es,map[,c("GEOID","ZCTA5")],by.x="geoid",by.y="GEOID")
       es<-es[!duplicated(es)&!is.na(es$ZCTA5)&es$ZCTA5!="",]
-      es<-es[,!names(es) %in% c("GEOID","geoid","year","state")]%>%
+      es<-es[,!names(es) %in% c("GEOID","geoid","geography","year","state")]%>%
         group_by(ZCTA5)%>%
         summarise_each(funs=c(sum))
     }
     es$geography<-es$ZCTA5
+    es<-es[,!names(es) %in% c("ZCTA5")]
   }
   
   lodes<-as.data.frame(es[,!names(es) %in% c("tract","geoid","GEOID","ZCTA5")])
